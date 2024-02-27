9062ff5287d8d04f7f3955e5d6af60aa709e5c61
jerozapata <159493286+jerozapata@users.noreply.github.com>
2024-02-26 22:21:01 -0500

    toma 2

--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -4,11 +4,15 @@ public class Marca {
 	private String nombre;
 	
 	public Marca(String nombre) {
-		this.nombre = nombre;
+		this.setNombre(nombre);
 	}
 	
 	public String getNombre() {
 		return this.nombre;
 	}
 	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+		
+	}
 }
